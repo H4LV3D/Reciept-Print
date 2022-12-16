d91c0a4b56c49e71ca28ec04e41c503a0111f71d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-16 17:33:53 +0100

    changed 2225 files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var today = new Date();
 var date =
   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 document.getElementById("date").value = date;
+document.getElementById("today").innerText = date;
 
 localStorage.setItem("patientNo", 01);
 
@@ -88,3 +89,34 @@ function printme() {
   let Reciept = "reciept";
   saveDiv(Reciept);
 }
+
+// TIME
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if (hh === 0) {
+    hh = 12;
+  }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
+
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").value = time;
+  document.getElementById("time").innerText = time;
+  let t = setTimeout(function () {
+    currentTime();
+  }, 1000);
+}
+
+currentTime();
