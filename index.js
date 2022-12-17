7e726dd654f2b882a012bac9a2f146f2beed4881
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-12-17 23:33:38 +0100

    changed 927 files

--- index.js
+++ index.js
@@ -46,61 +46,77 @@ app.post("/login", (req, res) => {
       username === "gift" ? database.user1.password : database.user2.password;
 
     const hashedPassword = bcrypt.hashSync(password, 10);
-
-    // Compare the hashed password to the original password
     if (bcrypt.compareSync(password, hashedPassword)) {
       console.log("success");
       res.status(200).json({
         success: `Welcome Home`,
       });
     } else {
-      // The password is incorrect
       res.status(401).send("Incorrect password");
     }
   } else {
-    // The username is invalid
     res.status(401).send("Invalid username");
   }
 });
 
 app.get("/reciept", (req, res) => {
   const folderPath = path.join(__dirname, "Reciept");
-  console.log(folderPath);
   const files = fs
     .readdirSync(folderPath)
     .filter((file) => file.endsWith(".pdf"));
+  console.log(files);
 
-  files.forEach((file) => {
+  let fileName = [];
+  let fileSize = [];
+  let modifiedDate = [];
+  let contexts = [];
+
+  files.forEach(async (file) => {
     const filePath = path.join(folderPath, file);
     const stats = fs.statSync(filePath);
-    const fileSize = stats.size;
-    const modifiedDate = stats.mtime;
+    fileName.push(file.replace(/\.[^/.]+$/, ""));
+    fileSize.push(stats.size);
+    modifiedDate.push(stats.mtime);
 
-    const data = new Uint8Array(fs.readFileSync(filePath));
-    global.document = new JSDOM().window.document;
-    let context;
-    pdfjs.getDocument(data).promise.then(function (pdf) {
-      var numPages = pdf.numPages;
-      for (var i = 1; i <= numPages; i++) {
-        pdf.getPage(i).then(function (page) {
-          var scale = 1.5;
-          var viewport = page.getViewport({ scale: scale });
-          var container = document.getElementById("pdf-container");
-          var canvas = document.createElement("canvas");
-          context = canvas.getContext("2d");
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-          page.render({ canvasContext: context, viewport: viewport });
-          return context;
+    function getPageText(pageNum, PDFDocumentInstance) {
+      return new Promise(function (resolve, reject) {
+        PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
+          pdfPage.getTextContent().then(function (textContent) {
+            var textItems = textContent.items;
+            var finalString = "";
+            for (var i = 0; i < textItems.length; i++) {
+              var item = textItems[i];
+              finalString += item.str + " ";
+            }
+            resolve(finalString);
+          });
         });
+      });
+    }
+
+    let PDF_URL = filePath;
+
+    let text = pdfjs.getDocument(PDF_URL).promise.then(
+      function (PDFDocumentInstance) {
+        var pageNumber = 1;
+        getPageText(pageNumber, PDFDocumentInstance).then(function (textPage) {
+          contexts.push(textPage);
+        });
+      },
+      function (reason) {
+        console.error(reason);
       }
-    });
+    );
+  });
+
+  setTimeout(() => {
     res.status(200).json({
+      fileName,
       fileSize,
       modifiedDate,
-      context,
+      contexts,
     });
-  });
+  }, 1000);
 });
 
 app.get("/", function (req, res) {
