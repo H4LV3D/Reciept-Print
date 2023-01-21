587b06db4b2876e4dfd7bc3b93dc8b06397181a7
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-01-21 18:52:42 +0100

    changed 3 files

---
+++ Server/models/patient.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  symptoms: [
+    {
+      type: String,
+    },
+  ],
+  medical_history: {
+    type: String,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("patient", patientSchema);
