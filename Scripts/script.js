74dae9994928a049cde7cb72c2c1ead57d2ab9df
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-02-28 09:15:07 +0100

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -114,31 +114,27 @@ function printme() {
 
 // TIME
 function currentTime() {
-  let date = new Date();
+  const date = new Date();
   let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
   let session = "AM";
 
   if (hh === 0) {
     hh = 12;
-  }
-  if (hh > 12) {
+  } else if (hh > 12) {
     hh = hh - 12;
     session = "PM";
   }
 
-  hh = hh < 10 ? "0" + hh : hh;
-  mm = mm < 10 ? "0" + mm : mm;
-  ss = ss < 10 ? "0" + ss : ss;
+  const formattedHour = hh < 10 ? `0${hh}` : hh;
+  const formattedMinute = date.getMinutes().toString().padStart(2, "0");
+  const formattedSecond = date.getSeconds().toString().padStart(2, "0");
 
-  let time = hh + ":" + mm + ":" + ss + " " + session;
+  const time = `${formattedHour}:${formattedMinute}:${formattedSecond} ${session}`;
 
   document.getElementById("clock").value = time;
   document.getElementById("time").innerText = time;
-  let t = setTimeout(function () {
-    currentTime();
-  }, 1000);
+
+  setTimeout(currentTime, 1000);
 }
 
 currentTime();
